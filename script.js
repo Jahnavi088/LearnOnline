8c9449df1accd4c14e531e525b021ec39b5580a3
jahnavi <jahnavibolisetti0508@gmail.com>
2025-01-14 20:03:49 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+
+document.querySelector('.collapse-btn').addEventListener('click', function() {
+    const leftMenu = document.querySelector('.left-menu');
+    leftMenu.classList.toggle('collapsed');
+  });
+  
